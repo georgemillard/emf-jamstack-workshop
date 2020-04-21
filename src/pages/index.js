62dac633f8d6bd220463354ebac2850e42d06959
George Millard <george.millard@ellenmacarthurfoundation.org>
2020-04-21 14:40:28 +0100

    test branch

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ const Index = () => {
       <h1>EMF CI/CD Workshop</h1>
       <p>This is the project for our workshop on Gatsby Cloud and GitHub Actions</p>
       <p>Use this project for the workshop!!!</p>
+      <p>Test branch</p>
     </>
   );
 };
